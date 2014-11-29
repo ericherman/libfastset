8f018d8f8c272cf461b36d50242ac79d21084819
Eric Herman <eric@freesa.org>
2014-11-29 10:16:36 +0100

    make c++ friendly

--- fastset.h
+++ fastset.h
@@ -4,6 +4,10 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct fastset_t;
 
 struct fastset_t *fastset_create(size_t max_value);
@@ -15,5 +19,9 @@ void fastset_remove(struct fastset_t *fastset, size_t value);
 void fastset_clear(struct fastset_t *fastset);
 
 void fastset_foreach(struct fastset_t *fastset, void (*pt2Func) (size_t each));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _FASTSET_H_ */
