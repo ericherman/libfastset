934454841315668a8fba212ffab3405f04cf1494
Eric Herman <eric@freesa.org>
2014-11-29 09:52:01 +0100

    sketch out an interface

---
+++ fastset.h
@@ -0,0 +1,19 @@
+/* fastset.h */
+#ifndef _FASTSET_H_
+#define _FASTSET_H_
+
+#include <stddef.h>
+
+struct fastset_t;
+
+struct fastset_t *fastset_create(size_t max_value);
+void fastset_free(struct fastset_t *fastset);
+
+void fastset_add(struct fastset_t *fastset, size_t value);
+int fastset_contains(struct fastset_t *fastset, size_t value);
+void fastset_remove(struct fastset_t *fastset, size_t value);
+void fastset_clear(struct fastset_t *fastset);
+
+void fastset_foreach(struct fastset_t *fastset, void (*pt2Func) (size_t each));
+
+#endif /* _FASTSET_H_ */
