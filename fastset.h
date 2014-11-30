554e3da136fc1b8184636a9961bfd606994151a2
Eric Herman <eric@freesa.org>
2014-11-30 09:13:12 +0100

    add some docs

--- fastset.h
+++ fastset.h
@@ -10,18 +10,44 @@ extern "C" {
 
 struct fastset_t;
 
+/*
+ * The "max_value" argument is the largest legal value this set will be
+ * prepared to handle.
+ *
+ * Mmemory allocated is approximately: 2 * max_value * sizeof(size_t).
+ */
 struct fastset_t *fastset_create(size_t max_value);
+
+/* must be called for destruction to avoid leaking memory */
 void fastset_free(struct fastset_t *fastset);
 
+/* fastset_add will return 0 if the value is too large for the set */
 int fastset_add(struct fastset_t *fastset, size_t value);
+
+/* fastset_contains returns 0 if the value is not contained in the set */
 int fastset_contains(struct fastset_t *fastset, size_t value);
+
+/*
+ * The fastset_remove operation removes the value from the set.
+ * Value removal changes the iteration order.
+ */
 void fastset_remove(struct fastset_t *fastset, size_t value);
+
+/* fastset_size returns the current number of values in the set */
 size_t fastset_size(struct fastset_t *fastset);
-size_t fastset_max(struct fastset_t *fastset);
+
+/* fastset_clear resets to an empty set */
 void fastset_clear(struct fastset_t *fastset);
 
+/*
+ * If no fastset_remove() operations have occured, iteration will
+ * be in element insertion order.
+ */
 void fastset_foreach(struct fastset_t *fastset,
 		     void (*func) (size_t each, void *arg), void *arg);
+
+/* the maxium legal value this set is prepared to handle */
+size_t fastset_max(struct fastset_t *fastset);
 
 #ifdef __cplusplus
 }
